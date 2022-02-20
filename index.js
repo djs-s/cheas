2ad0151b9678b72e33bb6c457fda9faaab57883d
djs-s <100082894+djs-s@users.noreply.github.com>
2022-02-20 17:57:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,11 @@ module.exports = {
     addOne: async (number) => {
         console.log(number += 1)
         return number += 1;
-    }
+    },
+    green: async (colors) => {
+        "GREEN"
+    },
+    red: async (colors) => {
+        "RED"
+    },
 }
